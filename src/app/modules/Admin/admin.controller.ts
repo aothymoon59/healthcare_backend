d797cd0c92acab51229718babd4a5edb5cc7cf69
aothymoon59 <aothymoon59@gmail.com>
2024-11-30 10:30:23 +0600

    :sparkles: Retrieving All Admin Data from the Database

---
+++ src/app/modules/Admin/admin.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { AdminService } from "./admin.service";
+
+const getAllFromDB = async (req: Request, res: Response) => {
+  const result = await AdminService.getAllFromDB();
+  try {
+    console.log(result);
+    res.status(200).json({
+      success: true,
+      message: "Admin data fetched successfully",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || "Something went wrong",
+      error: err,
+    });
+  }
+};
+
+export const AdminController = {
+  getAllFromDB,
+};
--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -11,7 +11,7 @@ const createAdmin = async (req: Request, res: Response) => {
       message: "Admin created successfully",
       data: result,
     });
-  } catch (err) {
+  } catch (err: any) {
     res.status(500).json({
       success: false,
       message: err?.name || "Something went wrong",
