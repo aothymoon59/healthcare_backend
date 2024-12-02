29ea35d026876bbf2a602dc34a4d0f375959f01f
aothymoon59 <aothymoon59@gmail.com>
2024-12-02 14:26:10 +0600

    :sparkles: Generating Access Tokens for User Login

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,5 +1,29 @@
+import prisma from "../../../shared/prisma";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 const loginUser = async (payload: { email: string; password: string }) => {
-  console.log("user logged in", payload);
+  const userData = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: payload.email,
+    },
+  });
+
+  const isCorrectPassword: boolean = await bcrypt.compare(
+    payload.password,
+    userData.password
+  );
+
+  const accessToken = jwt.sign(
+    {
+      email: userData.email,
+      role: userData.role,
+    },
+    "sdjghsfjghfdghgdf",
+    { algorithm: "HS256", expiresIn: "30m" }
+  );
+
+  return userData;
 };
 
 export const AuthServices = {
