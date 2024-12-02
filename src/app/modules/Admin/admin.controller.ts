42753ae22e4b21ed7314f1977c81c5319c8cfea9
aothymoon59 <aothymoon59@gmail.com>
2024-12-02 13:01:23 +0600

    :construction: Implementing Request Validation Middleware: Part 1

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,19 +1,10 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
+import { Request, RequestHandler, Response } from "express";
 import { AdminService } from "./admin.service";
 import { pick } from "../../../shared/pick";
 import { adminFilterableFields } from "./admin.constant";
 import { sendResponse } from "../../../shared/sendResponse";
 import { StatusCodes } from "http-status-codes";
-
-const catchAsync = (fn: RequestHandler) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await fn(req, res, next);
-    } catch (err: any) {
-      next(err);
-    }
-  };
-};
+import { catchAsync } from "../../../shared/catchAsync";
 
 const getAllFromDB: RequestHandler = catchAsync(async (req, res) => {
   const filters = pick(req.query, adminFilterableFields);
--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,11 +1,16 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { AdminController } from "./admin.controller";
 
 const router = express.Router();
 
+const validateRequest = (req: Request, res: Response, next: NextFunction) => {
+  console.log("Checker Middleware", req.body);
+  next();
+};
+
 router.get("/", AdminController.getAllFromDB);
 router.get("/:id", AdminController.getByIdFromDB);
-router.patch("/:id", AdminController.updateIntoDB);
+router.patch("/:id", validateRequest, AdminController.updateIntoDB);
 router.delete("/:id", AdminController.deleteFromDB);
 router.delete("/soft/:id", AdminController.softDeleteFromDB);
 
