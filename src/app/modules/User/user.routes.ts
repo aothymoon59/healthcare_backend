0f9deb1f35d87aa14396e66aa676befc820328c9
aothymoon59 <aothymoon59@gmail.com>
2024-12-05 12:09:33 +0600

    :sparkles: Validating User Data and Inserting Data in Database with Image Link

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { UserController } from "./user.controller";
 import { UserRole } from "@prisma/client";
 import { auth } from "../../middlewares/auth";
 import { fileUploader } from "../../../helpers/fileUploader";
+import { userValidation } from "./user.validation";
 
 const router = express.Router();
 
@@ -10,7 +11,10 @@ router.post(
   "/",
   auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
   fileUploader.upload.single("file"),
-  UserController.createAdmin
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = userValidation.createAdmin.parse(JSON.parse(req.body.data));
+    return UserController.createAdmin(req, res, next);
+  }
 );
 
 export const UserRoutes = router;
