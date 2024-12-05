11c5c956665a2fa8ce7b83b04b98e1992e47ee29
aothymoon59 <aothymoon59@gmail.com>
2024-12-05 10:33:42 +0600

    :sparkles: Uploading Images to Current Working Directory with Multer

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -2,12 +2,14 @@ import express from "express";
 import { UserController } from "./user.controller";
 import { UserRole } from "@prisma/client";
 import { auth } from "../../middlewares/auth";
+import { fileUploader } from "../../../helpers/fileUploader";
 
 const router = express.Router();
 
 router.post(
   "/",
   auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  fileUploader.upload.single("file"),
   UserController.createAdmin
 );
 
---
+++ src/helpers/fileUploader.ts
@@ -0,0 +1,23 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(process.cwd(), "uploads"));
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.originalname.split(".")[0] +
+        "-" +
+        Date.now() +
+        path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+export const fileUploader = {
+  upload,
+};
