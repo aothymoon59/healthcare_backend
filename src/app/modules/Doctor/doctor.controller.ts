8930a60002c654ccc53c19090ccc9780ad4a2388
aothymoon59 <aothymoon59@gmail.com>
2024-12-10 11:28:05 +0600

    :sparkles: doctor controller added

--- src/app/modules/Doctor/doctor.controller.ts
+++ src/app/modules/Doctor/doctor.controller.ts
@@ -1,17 +1,18 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
-import sendResponse from "../../../shared/sendResponse";
-import httpStatus from "http-status";
-import catchAsync from "../../../shared/catchAsync";
+import { Request, Response } from "express";
 import { DoctorService } from "./doctor.service";
-import pick from "../../../shared/pick";
+// import pick from "../../../shared/pick";
 import { doctorFilterableFields } from "./doctor.constants";
+import { sendResponse } from "../../../shared/sendResponse";
+import { pick } from "../../../shared/pick";
+import { catchAsync } from "../../../shared/catchAsync";
+import { StatusCodes } from "http-status-codes";
 
 const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
   const filters = pick(req.query, doctorFilterableFields);
   const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
   const result = await DoctorService.getAllFromDB(filters, options);
   sendResponse(res, {
-    statusCode: httpStatus.OK,
+    statusCode: StatusCodes.OK,
     success: true,
     message: "Doctors retrieval successfully",
     meta: result.meta,
@@ -23,7 +24,7 @@ const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   const result = await DoctorService.getByIdFromDB(id);
   sendResponse(res, {
-    statusCode: httpStatus.OK,
+    statusCode: StatusCodes.OK,
     success: true,
     message: "Doctor retrieval successfully",
     data: result,
@@ -35,7 +36,7 @@ const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
   const result = await DoctorService.updateIntoDB(id, req.body);
 
   sendResponse(res, {
-    statusCode: httpStatus.OK,
+    statusCode: StatusCodes.OK,
     success: true,
     message: "Doctor data updated!",
     data: result,
@@ -46,7 +47,7 @@ const deleteFromDB = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   const result = await DoctorService.deleteFromDB(id);
   sendResponse(res, {
-    statusCode: httpStatus.OK,
+    statusCode: StatusCodes.OK,
     success: true,
     message: "Doctor deleted successfully",
     data: result,
@@ -57,7 +58,7 @@ const softDelete = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   const result = await DoctorService.softDelete(id);
   sendResponse(res, {
-    statusCode: httpStatus.OK,
+    statusCode: StatusCodes.OK,
     success: true,
     message: "Doctor soft deleted successfully",
     data: result,
