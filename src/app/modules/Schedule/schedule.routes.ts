c73d8be3889e34ecf2f411d5487f520ef20e17ad
aothymoon59 <aothymoon59@gmail.com>
2024-12-18 13:26:31 +0600

    :sparkles: schedule getByIdFromDB route added

--- src/app/modules/Schedule/schedule.routes.ts
+++ src/app/modules/Schedule/schedule.routes.ts
@@ -6,6 +6,13 @@ import { UserRole } from "@prisma/client";
 const router = express.Router();
 
 router.get("/", auth(UserRole.DOCTOR), ScheduleController.getAllFromDB);
+
+router.get(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR),
+  ScheduleController.getByIdFromDB
+);
+
 router.post(
   "/",
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
