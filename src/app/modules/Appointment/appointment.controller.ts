6b2ee1cb9a0d7402f21eb3175be91ef600e05c72
aothymoon59 <aothymoon59@gmail.com>
2024-12-18 15:46:34 +0600

    :construction: appointment work in progress

---
+++ src/app/modules/Appointment/appointment.controller.ts
@@ -0,0 +1,23 @@
+import { StatusCodes } from "http-status-codes";
+import { sendResponse } from "../../../shared/sendResponse";
+import { catchAsync } from "../../../shared/catchAsync";
+import { AppointmentServices } from "./appointment.service";
+import { Request, Response } from "express";
+import { IAuthUser } from "../../interfaces/common";
+
+const createAppointment = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const user = req.user;
+    const result = await AppointmentServices.createAppointment();
+    sendResponse(res, {
+      statusCode: StatusCodes.OK,
+      success: true,
+      message: "Appointment booked successfully!",
+      data: result,
+    });
+  }
+);
+
+export const AppointmentController = {
+  createAppointment,
+};
---
+++ src/app/modules/Appointment/appointment.service.ts
@@ -0,0 +1,5 @@
+const createAppointment = async () => {
+  console.log("appointment!!!");
+};
+
+export const AppointmentServices = { createAppointment };
