4214c9d501fb070a1e4777d9533fc383aa864d18
aothymoon59 <aothymoon59@gmail.com>
2024-12-19 10:23:57 +0600

    :sparkles: Making an Appointment: Part 1

--- src/app/modules/Appointment/appointment.service.ts
+++ src/app/modules/Appointment/appointment.service.ts
@@ -1,5 +1,13 @@
-const createAppointment = async () => {
-  console.log("appointment!!!");
+import prisma from "../../../shared/prisma";
+import { IAuthUser } from "../../interfaces/common";
+
+const createAppointment = async (user: IAuthUser, payload: any) => {
+  const patientData = await prisma.patient.findUniqueOrThrow({
+    where: {
+      email: user?.email,
+    },
+  });
+  console.log("appointment!!!", payload);
 };
 
 export const AppointmentServices = { createAppointment };
