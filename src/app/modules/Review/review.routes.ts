4a2b7a77a30a9d7528dff8700d727492d37ccc28
aothymoon59 <aothymoon59@gmail.com>
2024-12-31 16:27:14 +0600

    :sparkles: create review route added

---
+++ src/app/modules/Review/review.routes.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { ReviewController } from "./review.controller";
+import { UserRole } from "@prisma/client";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { ReviewValidation } from "./review.validation";
+import { auth } from "../../middlewares/auth";
+
+const router = express.Router();
+
+router.post(
+  "/",
+  auth(UserRole.PATIENT),
+  validateRequest(ReviewValidation.create),
+  ReviewController.insertIntoDB
+);
+
+export const ReviewRoutes = router;
