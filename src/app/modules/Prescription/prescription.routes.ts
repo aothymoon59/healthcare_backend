9c4f70135d8bd6d39bb1c39de132be37e4210278
aothymoon59 <aothymoon59@gmail.com>
2024-12-31 16:12:44 +0600

    :sparkles: added zod validation schema for creating prescription

--- src/app/modules/Prescription/prescription.routes.ts
+++ src/app/modules/Prescription/prescription.routes.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { PrescriptionController } from "./prescription.controller";
 import { auth } from "../../middlewares/auth";
 import { UserRole } from "@prisma/client";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { PrescriptionValidation } from "./prescription.validation";
 
 const router = express.Router();
 
@@ -17,6 +19,11 @@ router.get(
   PrescriptionController.patientPrescription
 );
 
-router.post("/", auth(UserRole.DOCTOR), PrescriptionController.insertIntoDB);
+router.post(
+  "/",
+  auth(UserRole.DOCTOR),
+  validateRequest(PrescriptionValidation.create),
+  PrescriptionController.insertIntoDB
+);
 
 export const PrescriptionRoutes = router;
