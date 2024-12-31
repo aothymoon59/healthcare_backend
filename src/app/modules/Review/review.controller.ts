990e15d11fa513905a89c51e16c3533430849199
aothymoon59 <aothymoon59@gmail.com>
2024-12-31 16:34:26 +0600

    :sparkles: get all reviews controller added

--- src/app/modules/Review/review.controller.ts
+++ src/app/modules/Review/review.controller.ts
@@ -4,6 +4,8 @@ import { IAuthUser } from "../../interfaces/common";
 import { catchAsync } from "../../../shared/catchAsync";
 import { sendResponse } from "../../../shared/sendResponse";
 import { StatusCodes } from "http-status-codes";
+import { pick } from "../../../shared/pick";
+import { reviewFilterableFields } from "./review.constants";
 
 const insertIntoDB = catchAsync(
   async (req: Request & { user?: IAuthUser }, res: Response) => {
@@ -21,6 +23,20 @@ const insertIntoDB = catchAsync(
   }
 );
 
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, reviewFilterableFields);
+  const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+  const result = await ReviewService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "Reviews retrieval successfully",
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const ReviewController = {
   insertIntoDB,
+  getAllFromDB,
 };
