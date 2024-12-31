379d6a7e44421b3441cb7c828263cb4f60e50f19
aothymoon59 <aothymoon59@gmail.com>
2024-12-31 15:32:04 +0600

    :sparkles: patientPrescription controller added

--- src/app/modules/Prescription/prescription.controller.ts
+++ src/app/modules/Prescription/prescription.controller.ts
@@ -4,6 +4,7 @@ import { IAuthUser } from "../../interfaces/common";
 import { sendResponse } from "../../../shared/sendResponse";
 import { catchAsync } from "../../../shared/catchAsync";
 import { StatusCodes } from "http-status-codes";
+import { pick } from "../../../shared/pick";
 
 const insertIntoDB = catchAsync(
   async (req: Request & { user?: IAuthUser }, res: Response) => {
@@ -21,6 +22,25 @@ const insertIntoDB = catchAsync(
   }
 );
 
+const patientPrescription = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const user = req.user;
+    const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+    const result = await PrescriptionService.patientPrescription(
+      user as IAuthUser,
+      options
+    );
+    sendResponse(res, {
+      statusCode: StatusCodes.OK,
+      success: true,
+      message: "Prescription fetched successfully",
+      meta: result.meta,
+      data: result.data,
+    });
+  }
+);
+
 export const PrescriptionController = {
   insertIntoDB,
+  patientPrescription,
 };
