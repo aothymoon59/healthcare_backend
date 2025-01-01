ff05996423ac170c9238c5091ca2d4f5426682c3
aothymoon59 <aothymoon59@gmail.com>
2025-01-01 11:04:17 +0600

    :sparkles: Retrieving Dashboard Metadata: Part 1

--- src/app/modules/Meta/meta.controller.ts
+++ src/app/modules/Meta/meta.controller.ts
@@ -3,10 +3,12 @@ import { sendResponse } from "../../../shared/sendResponse";
 import { catchAsync } from "../../../shared/catchAsync";
 import { StatusCodes } from "http-status-codes";
 import { MetaService } from "./meta.service";
+import { IAuthUser } from "../../interfaces/common";
 
 const fetchDashboardMetaData = catchAsync(
-  async (req: Request, res: Response) => {
-    const result = await MetaService.fetchDashboardMetaData();
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const user = req.user;
+    const result = await MetaService.fetchDashboardMetaData(user as IAuthUser);
     sendResponse(res, {
       statusCode: StatusCodes.OK,
       success: true,
--- src/app/modules/Meta/meta.service.ts
+++ src/app/modules/Meta/meta.service.ts
@@ -1,5 +1,36 @@
-const fetchDashboardMetaData = async () => {
-  console.log("Dashboard meta data is being fetched");
+import { UserRole } from "@prisma/client";
+import { IAuthUser } from "../../interfaces/common";
+
+const fetchDashboardMetaData = async (user: IAuthUser) => {
+  switch (user?.role) {
+    case UserRole.SUPER_ADMIN:
+      getSuperAdminMetaData();
+      break;
+    case UserRole.ADMIN:
+      getAdminMetaData();
+      break;
+    case UserRole.DOCTOR:
+      getDoctorMetaData();
+      break;
+    case UserRole.PATIENT:
+      getPatientMetaData();
+      break;
+    default:
+      throw new Error("You are not authorized");
+  }
+};
+
+const getSuperAdminMetaData = async () => {
+  console.log("super admin");
+};
+const getAdminMetaData = async () => {
+  console.log("admin");
+};
+const getDoctorMetaData = async () => {
+  console.log("doctor");
+};
+const getPatientMetaData = async () => {
+  console.log("patient");
 };
 
 export const MetaService = {
